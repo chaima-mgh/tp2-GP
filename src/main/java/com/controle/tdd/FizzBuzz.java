6c090216799305a21e18753d17f7c54e7868cfac
chaima-mgh <chaimamgh777@gmail.com>
2025-12-14 18:55:46 +0100

    on a fixé le cas de devision sur 3et 5

--- src/main/java/com/controle/tdd/FizzBuzz.java
+++ src/main/java/com/controle/tdd/FizzBuzz.java
@@ -3,7 +3,10 @@ package com.controle.tdd;
 public class FizzBuzz {
         public static String de(int nbre){
             String S;
-            if(nbre % 3 == 0  ){
+            if(nbre % 3 == 0 && nbre % 5 == 0 ){
+                S="FizzBuzz";
+            }
+            else if(nbre % 3 == 0  ){
                 S ="Fizz";
             }else if(nbre % 5 == 0 ){
                 S="Buzz";
